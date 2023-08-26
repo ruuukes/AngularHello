aca05fbd5baf6215e8e3fef1d2f25ed973e149ea
Ariunchimeg Sanchir <aruka@AriunchimegsAir.lan>
2023-08-26 00:12:27 -0700

    tour of herooes stage 5 done

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -15,7 +15,11 @@ export class HeroService {
     const heroes = of(HEROES);
     this.messagesService.add('HeroService: fetched heroes');
     return heroes;
+  }
 
-
+  getHero(id : number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messagesService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,7 +9,7 @@ import { MessageService } from '../messages.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent{
 
   selectedHero?: Hero;
 
@@ -19,11 +19,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
